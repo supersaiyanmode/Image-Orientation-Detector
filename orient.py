010c2f3df181b83520d54f75f3d749e460de9432
Srivatsan <supersaiyanmode.rox@gmail.com>
2015-12-13 05:12:25 -0500

    Temp. commit

--- orient.py
+++ orient.py
@@ -7,7 +7,10 @@ from itertools import izip, imap
 
 sigmoid = lambda u: 1.0 / (1 + math.exp(-u))
 sigmoid_ = lambda u: sigmoid(u) * (1 - sigmoid(u))
-tanh = lambda u: 2 * Neuron.sigmoid(2 * u) - 1
+tanh = lambda u: math.tanh(u)
+tanh_ = lambda u: 1 - math.tanh(u)**2
+new = lambda u: 1.7159*math.tanh(2.0 * u / 3.0) 
+new_ = lambda u: 1.14393 * tanh_(2.0 * u / 3.0)
 
 def dot(x,y):
     if len(x) != len(y):
@@ -35,56 +38,81 @@ def load_data_file(fileName):
     return [TestData(line[0], int(line[1]), map(int,line[2:])) for line in map(lambda x: x.strip().split(), open(fileName))]
 
 
-def train_neural_network(train_data, hiddenCount, fn, fn_, alpha):
-    featureLength = len(train_data[0].data)
-    classLength = 4
-    weights = [None, 
-        [[random.random() for __ in range(featureLength)] for _ in range(hiddenCount)], 
-        [[random.random() for __ in range(hiddenCount)] for _ in range(classLength)]
-    ]
-    errors = [
-        [0] * featureLength,
-        [0] * hiddenCount,
-        [0] * classLength,
-    ]
-    o = lambda x: [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]][x/90]
-    for _ in range(50):
-        for input_set, output_set in imap(lambda x: (x.data, o(x.orientation)), train_data):
-            a = [input_set, [0]*hiddenCount, [0]*classLength]
-            inp = [None, [0]*hiddenCount, [0]*classLength]
+class NeuralNetwork(object):
+    def __init__(self, train_data, hiddenCount, alpha, fn, fn_):
+        rand_weight = lambda : random.random()*2 - 1
+        self.num_nodes = [len(train_data[0].data)+1, hiddenCount, 4]
+        self.activations = [[0] * x for x in self.num_nodes]
+        self.weights = [
+            [[rand_weight() for __ in range(self.num_nodes[1])] for _ in range(self.num_nodes[0])], 
+            [[rand_weight() for __ in range(self.num_nodes[2])] for _ in range(self.num_nodes[1])]
+        ]
+        self.fn = fn
+        self.fn_ = fn_
+        self.alpha = alpha
 
-            for l in [1, 2]:
-                for index, neuron_weights in enumerate(weights[l]):
-                    inp[l][index] = dot(neuron_weights, a[l-1])
-                    a[l][index] = fn(inp[l][index])
+    def train(self, train_data):
+        o = lambda x: [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]][x/90]
 
-            #Propagate deltas backward.
-            for j in range(classLength):
-                errors[2][j] = fn_(inp[-1][j]) * (output_set[j] - a[-1][j])
+        for iteration in range(50):
+            print "Iteration:",iteration
+            sum_errors = []
+            for input_set, output_set in imap(lambda x: (x.data, o(x.orientation)), train_data):
+                result = self.solve(input_set)
+                
+                errors = [[0] * self.num_nodes[1], [0] * self.num_nodes[2]]
+                #Layer 2-3 errors
+                errors[1] = [self.fn_(self.activations[2][x]) * (output_set[x] - self.activations[2][x]) \
+                                for x in range(self.num_nodes[2])]
+                
+                #Layer 1-2 errors
+                for neuron_index in range(self.num_nodes[1]):
+                    error = 0.0
+                    for x in range(self.num_nodes[2]):
+                        error += errors[1][x] * self.weights[1][neuron_index][x]
+                    #sum(errors[2][x]*self.weights[2][neuron_index][x] for x in range(self.num_nodes[2]))
+                    errors[0][neuron_index] = self.fn_(self.activations[1][neuron_index]) * error
 
-            for l in [1, 0]:
-                for index, neuron_weights  in enumerate(weights[l+1]):
-                    #import pdb; pdb.set_trace()
-                    temp = 0
-                    for index1, neuron_weights1 in enumerate(weights[l+1]):
-                        temp += weights[l+1][index1][index] * errors[l+1][index1]
+                #Update weights 2-3
+                for neuron_index1 in range(self.num_nodes[1]):
+                    for neuron_index2 in range(self.num_nodes[2]):
+                        self.weights[1][neuron_index1][neuron_index2] += self.alpha * \
+                                errors[1][neuron_index2] * self.activations[1][neuron_index1]
 
-                    errors[l][index] = fn_(inp[l+1][index]) * temp
+                #Update weight 1-2
+                for neuron_index1 in range(self.num_nodes[0]):
+                    for neuron_index2 in range(self.num_nodes[1]):
+                        self.weights[0][neuron_index1][neuron_index2] += self.alpha * \
+                                errors[0][neuron_index2] * self.activations[0][neuron_index1]
+                
+                sum_errors.append(sum((x-y)**2 for x,y in zip(output_set, result)))
+            print "Avg. Error:", sum(sum_errors) / float(len(sum_errors))
 
-            for l in [1, 2]:
-                for neuron_index, neuron_weights in enumerate(weights[l]):
-                    weights[l][neuron_index] += alpha * a[l][neuron_index] * errors[l][index]
+    def solve(self, input_data):
+        #Layer #1
+        self.activations[0] = input_data + [1]
 
-    return weights
+        #Layer #2
+        for node_index in range(self.num_nodes[1]):
+            total = 0.0
+            for x in range(self.num_nodes[0]):
+                total += self.activations[0][x] * self.weights[0][x][node_index]
+            self.activations[1][node_index] = self.fn(total)
+        #Layer #3
+        for node_index in range(self.num_nodes[2]):
+            total = sum(self.activations[1][x] * self.weights[1][x][node_index] \
+                    for x in range(self.num_nodes[1]))
+            self.activations[2][node_index] = self.fn(total)
 
-def solve_neural_network(train_data, test_data, hiddenCount, fn=sigmoid, fn_=sigmoid_, alpha=0.2):
-    weights = train_neural_network(train_data, hiddenCount, fn=sigmoid, fn_=sigmoid_, alpha=alpha)
+        return list(self.activations[2])
 
-    for test in test_data:
-        input_arr = test
-        for l in [1,2]:
-            input_arr = [fn(dot(neuron_weights, input_arr)) for index, neuron_weights in enumerate(weights[l])]
-        yield [0,90,180,270][max(enumerate(input_arr), key=lambda x: x[1])[0]]
+def solve_neural_network(train_data, test_data, hiddenCount, fn=sigmoid, fn_=sigmoid_, alpha=0.5):
+    network = NeuralNetwork(train_data, hiddenCount, alpha=alpha, fn=new, fn_=new_)
+    print "Training Neural Network..."
+    network.train(train_data)
+    for input_data in test_data:
+        res = network.solve(input_data.data)
+        yield input_data, [0,90,180,270][max(enumerate(res), key=lambda x: x[1])[0]]
 
 def main():
     _, train_file, test_file, algorithm, param = sys.argv
